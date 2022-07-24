869c08ab86a6808f0961788b837f43efa503e506
asif hamzayev <asif.gamzayev@gmail.com>
2022-07-24 15:28:53 -0400

    AsifFirst Commit2

--- src/test/java/com/duobank/stepDefintions/AsifStepDefs.java
+++ src/test/java/com/duobank/stepDefintions/AsifStepDefs.java
@@ -6,34 +6,79 @@ import com.duobank.utilities.SeleniumUtils;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 public class AsifStepDefs {
     @When("I should click on mortgage application")
     public void i_should_click_on_mortgage_application() {
         AsifPage mortgageDetails = new AsifPage();
-        mortgageDetails.clickOnMortgage();
+        mortgageDetails.clickOnMortgage.click();
         Assert.assertEquals("http://qa-duobank.us-east-2.elasticbeanstalk.com/mortagage.php", Driver.getDriver().getCurrentUrl());
-
 
     }
 
     @Then("I need to fill preapproval details")
     public void i_need_to_fill_preapproval_details() {
         AsifPage mortgageDetails = new AsifPage();
-        mortgageDetails.clickNoRealtor();
-        mortgageDetails.clickNoLoanOfficer();
-        mortgageDetails.EnterEstimatePrice();
-        mortgageDetails.EnterDownPayment();
-
-
+        mortgageDetails.clickNoRealtor.click();
+        mortgageDetails.clickNoLoanOfficer.click();
+        mortgageDetails.EnterEstimatePrice.sendKeys("300000");
+        mortgageDetails.EnterDownPayment.sendKeys("60000");
 
     }
+
     @Then("I should push next button")
     public void i_should_push_next_button() {
         AsifPage mortgageDetails = new AsifPage();
-        mortgageDetails.clickOnNext();
-
-
+        mortgageDetails.clickOnNext.click();
+        SeleniumUtils.waitFor(3);
+        Assert.assertTrue( Driver.getDriver().findElement(By.xpath("//label[@for='proposalTitle1']")).isDisplayed());
 
     }
+    @Then("I should push next negative button")
+    public void i_should_push_next_negative_button() {
+        AsifPage mortgageDetails = new AsifPage();
+        mortgageDetails.clickOnNext.click();
+
+    }
+
+
+    @Then("I need to fill preapproval details with negative price")
+    public void i_need_to_fill_preapproval_details_with_negative_price() {
+        AsifPage mortgageDetails = new AsifPage();
+        mortgageDetails.EnterRealtorInfo();
+        mortgageDetails.EnterNegativeEstimatePrice.sendKeys("-300000");
+        mortgageDetails.EnterDownPayment.sendKeys("60000");
+        Assert.assertEquals("PLEASE ENTER A VALUE GREATER THAN OR EQUAL TO 1.", Driver.getDriver().findElement(By.id("estimatedprice-error")).getText());
+    }
+
+    @Then("I need to fill preapproval details with Realtor, Loan officer and Other Type Payment")
+    public void i_need_to_fill_preapproval_details_with_realtor_loan_officer_and_other_type_payment() {
+
+        AsifPage mortgageDetails = new AsifPage();
+        mortgageDetails.EnterRealtorInfo();
+        mortgageDetails.EnterEstimatePrice.sendKeys("300000");
+        mortgageDetails.EnterDownPayment.sendKeys("60000");
+        SeleniumUtils.scroll(0,200);
+        mortgageDetails.clickOnSourceOfDownPayment();
+        Assert.assertEquals("Other type of Down Payment", Driver.getDriver().findElement(By.xpath("//span[@title='Other type of Down Payment']")).getText());
+
+    }
+
+
+    @Then("I need to fill preapproval details with Realtor and Loan officer, with Additional Funds")
+    public void i_need_to_fill_preapproval_details_with_realtor_and_loan_officer_with_additional_funds() {
+        AsifPage mortgageDetails = new AsifPage();
+        mortgageDetails.EnterRealtorInfo();
+        mortgageDetails.EnterEstimatePrice.sendKeys("300000");
+        mortgageDetails.EnterDownPayment.sendKeys("60000");
+        SeleniumUtils.scroll(0,300);
+        mortgageDetails.clickOnSourceOfDownPayment();
+        mortgageDetails.EnterAdditionalFunds.sendKeys("50000");
+
+    }
+
+
+
 }
--- src/test/resources/AsifSprint4.feature
+++ src/test/resources/AsifSprint4.feature
@@ -1,15 +1,42 @@
-@regrestion
+@regression
 
 Feature: User sing up feature
   as an unregister user i should be able to sing up using singup page
 
   @smoke
-  Scenario: Registered user singin positive
+  Scenario: Registered user fill Preapproval application, without Realtor and Loan officer
     Given I am on the home page
     When I enter valid credentials.
     Then I should be able to sing in successfully
     When I should click on mortgage application
     Then I need to fill preapproval details
+    And I should push next button
+
+
+@smoke
+  Scenario: Registered user fill Preapproval application, with by entering negative number in Estimated Purchase Price section.
+    Given I am on the home page
+    When I enter valid credentials.
+    Then I should be able to sing in successfully
+    When I should click on mortgage application
+    Then I need to fill preapproval details with negative price
+    And I should push next negative button
+
+
+  Scenario: Registered user fill Preapproval application, with Realtor, Loan officer and Other Type Payment
+    Given I am on the home page
+    When I enter valid credentials.
+    Then I should be able to sing in successfully
+    When I should click on mortgage application
+    Then I need to fill preapproval details with Realtor, Loan officer and Other Type Payment
+    And I should push next button
+
+  Scenario: Registered user fill Preapproval application, Additional Funds
+    Given I am on the home page
+    When I enter valid credentials.
+    Then I should be able to sing in successfully
+    When I should click on mortgage application
+    Then I need to fill preapproval details with Realtor and Loan officer, with Additional Funds
     And I should push next button
 
 
