6c2f870c669b4cbc928c1bd186e448a54094ebc7
rahimrustamov <rustamov2611@gmail.com>
2022-07-25 16:01:06 -0400

    Rahims Commit

---
+++ src/test/java/com/duobank/pages/RahimsPage.java
@@ -0,0 +1,83 @@
+package com.duobank.pages;
+
+import com.duobank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class RahimsPage {
+
+        public RahimsPage(){
+            PageFactory.initElements(Driver.getDriver(),this);
+
+        }
+
+        @FindBy(xpath = "//label[@for='creditreport1']")
+        public WebElement clickYesCreditReport;
+
+        @FindBy(xpath = "//label[@for='creditreport2']")
+        public WebElement clickNoCreditReport;
+
+        @FindBy(xpath = "//a[@href='#next']")
+        public WebElement clickNext;
+
+
+        @FindBy(xpath = "//input[@ID='eConsentdeclarerFirstName']")
+        public WebElement inputFirstName;
+
+        @FindBy(xpath = "//input[@ID='eConsentdeclarerLastName']")
+        public WebElement inputLastName;
+
+        @FindBy(xpath = "//input[@ID='eConsentdeclarerEmail']")
+        public WebElement inputEmail;
+
+        @FindBy(xpath = "//input[@id='agree']")
+        public WebElement consentAgree;
+
+        @FindBy(xpath = "//input[@id='dontagree']")
+        public WebElement consentDontAgree;
+
+        @FindBy(xpath = "//a[@id='steps-uid-0-t-4']")
+        public WebElement clickCreditReport;
+
+        @FindBy(xpath = "//a[@id='steps-uid-0-t-5']")
+        public WebElement clickEconsent;
+
+        @FindBy(xpath = "//ul[@role='tablist']//li[@class='current']")
+        public WebElement checkIfCurrent;
+
+        @FindBy(xpath = "//ul[@role='tablist']//li[@class='done'][4]")
+        public WebElement checkIfCreditReportDone;
+
+        @FindBy(xpath = "//ul[@role='tablist']//li[@class='done'][5]")
+        public WebElement checkIfConsentDone;
+
+
+        @FindBy(xpath = "//label[@id='eConsentdeclarerEmail-error']")
+        public WebElement checkForError;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
--- src/test/java/com/duobank/stepDefintions/RahimStepDefs.java
+++ src/test/java/com/duobank/stepDefintions/RahimStepDefs.java
@@ -1,4 +1,94 @@
 package com.duobank.stepDefintions;
 
+import com.duobank.pages.RahimsPage;
+import com.duobank.pages.VafaPage;
+import com.duobank.utilities.SeleniumUtils;
+import com.github.javafaker.Faker;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+
 public class RahimStepDefs {
+
+    @Given("I am on the credit report page")
+    public void I_am_on_the_credit_report_page() {
+        RahimsPage rr = new RahimsPage();
+        SeleniumUtils.waitFor(3);
+        assert rr.checkIfCurrent.getAttribute("class").equals("current");
+    }
+
+    @When("I click yes to order credit report")
+    public void I_click_yes_to_order_credit_report() {
+        RahimsPage rr = new RahimsPage();
+        rr.clickYesCreditReport.click();
+    }
+
+    @Then("I should be able to click next button")
+    public void I_should_be_able_to_click_next_button() {
+        RahimsPage rr = new RahimsPage();
+        rr.clickNext.click();
+        SeleniumUtils.waitFor(3);
+
+    }
+
+    @When("I click No to order credit report")
+    public void I_click_No_to_order_credit_report() {
+        RahimsPage rr = new RahimsPage();
+        rr.clickNoCreditReport.click();
+    }
+
+
+    @Given("I am on the E-consent page")
+    public void iAmOnTheEConsentPage() {
+        RahimsPage rr = new RahimsPage();
+        SeleniumUtils.waitFor(3);
+        assert rr.checkIfCreditReportDone.getAttribute("class").equals("done");
+    }
+
+    @When("I input incorrect information and click Agree")
+    public void iInputIncorrectInformationAndClickAgree() {
+
+        RahimsPage rr = new RahimsPage();
+        SeleniumUtils.waitFor(3);
+        Faker fakeInfo = new Faker();
+        rr.inputFirstName.sendKeys(Keys.ENTER+fakeInfo.number().digits(4));
+        rr.inputLastName.sendKeys(Keys.ENTER+fakeInfo.number().digits(4));
+        rr.inputEmail.sendKeys(Keys.ENTER+fakeInfo.number().digits(4));
+        rr.consentAgree.click();
+
+    }
+
+    @Then("I should not be able to click next button")
+    public void iShouldNotBeAbleToClickNextButton() {
+        RahimsPage rr = new RahimsPage();
+        SeleniumUtils.waitFor(3);
+        rr.clickNext.click();
+        Assert.assertEquals(rr.checkForError.getText(),"Please enter a valid email address.");
+
+    }
+
+    @When("I input correct information and click Agree")
+    public void iInputCorrectInformationAndClickAgree() {
+        RahimsPage rr = new RahimsPage();
+        SeleniumUtils.waitFor(3);
+        Faker fakeInfo = new Faker();
+        rr.inputFirstName.sendKeys(Keys.ENTER+fakeInfo.name().firstName());
+        rr.inputLastName.sendKeys(Keys.ENTER+fakeInfo.name().lastName());
+        rr.inputEmail.sendKeys(Keys.ENTER+fakeInfo.internet().emailAddress());
+        rr.consentAgree.click();
+    }
+
+    @Then("I should be able to click next button")
+    public void iShouldBeAbleToClickNextButton() {
+        RahimsPage rr = new RahimsPage();
+        SeleniumUtils.waitFor(3);
+        assert rr.checkIfConsentDone.getAttribute("class").equals("done");
+
+    }
+
+
+
+
 }
