ac9df4ff3d434c413fad9bb72bab203e96eb429e
Halil Dikmen <halil.dkmn@gmail.com>
2022-07-28 16:25:57 -0400

    first commit

--- src/test/java/com/duobank/runners/CucumberRunner.java
+++ src/test/java/com/duobank/runners/CucumberRunner.java
@@ -8,17 +8,18 @@ import org.junit.runner.RunWith;
  @RunWith(Cucumber.class)
  @CucumberOptions(
 
-
+         tags ="@smoke" ,
         features = "src/test/resources", // copy the path from the content root ,path to the folder where features files are located
          glue = "com/duobank/stepDefintions",//copy the path from the source root
          stepNotifications = true,
          plugin = {
                 "pretty",
-                 "html:target/built-in-report/cucmber-html-report.html"
+                 "html:target/built-in-report/cucmber-html-report.html",
+                 "json:target/cucumber.json"
 
          }
 
            // ,dryRun = true
      )
-    public class HalilRunner {
+    public class CucumberRunner {
   }
