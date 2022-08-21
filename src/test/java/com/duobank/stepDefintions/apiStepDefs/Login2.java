249254365074e2556c40d7bd4ed2cff71db4c093
Lyle Bantilo <lylebcastos@gmail.com>
2022-08-21 14:39:37 -0400

    final commit 2

--- src/test/java/com/duobank/stepDefintions/apiStepDefs/Login2.java
+++ src/test/java/com/duobank/stepDefintions/apiStepDefs/Login2.java
@@ -1,26 +1,13 @@
 package com.duobank.stepDefintions.apiStepDefs;
 
 import com.duobank.stepDefintions.Endpoints;
-import com.duobank.utilities.ConfigReader;
-import com.github.javafaker.Faker;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
-import io.restassured.mapper.ObjectMapperType;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.hamcrest.Matcher;
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Login2 {
 
