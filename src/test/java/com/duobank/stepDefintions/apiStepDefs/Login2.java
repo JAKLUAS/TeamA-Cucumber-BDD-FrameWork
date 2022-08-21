f0db9dbf8b7ecc4f1f6eff029bc39bf6a38e68bc
Lyle Bantilo <lylebcastos@gmail.com>
2022-08-20 22:38:56 -0400

    2nd commit

--- src/test/java/com/duobank/stepDefintions/apiStepDefs/Login2.java
+++ src/test/java/com/duobank/stepDefintions/apiStepDefs/Login2.java
@@ -1,32 +1,40 @@
 package com.duobank.stepDefintions.apiStepDefs;
 
+import com.duobank.stepDefintions.Endpoints;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
-
+import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.not;
 
 public class Login2 {
 
+
+    Response response;
     @When("I send a Post request to the endpoint with wrong credentials")
     public void iSendAPostRequestToTheEndpointWithWrongCredentials() {
-         given().
-                header("Accept", "application/json").
-//                header("Content-Type", "application/json").
-        contentType(ContentType.JSON).
-                body("{\n" +
-                        "  \"email\": \"avril_lavigne@mail.com\",\n" +
-                        "  \"password\": \"avril1234\",\n" +
-                        "}").
-                when(). log().all().
-                post("/videogames").
-                then(). log().all().
-                statusCode(not(200)).
-                header("Content-Type", "application/json;charset=UTF-8").
-                header("connection", "close").
+         response = given().
+                        header("Accept", "application/json").
+                        contentType(ContentType.JSON).
+                        body("{\n" +
+                                "  \"email\": \"Jhony_deep5@gmail.com\",\n" +
+                                "  \"password\": \"jhony12345\",\n" +
+                                "}").
+                    when(). log().all().
+                        post(String.valueOf(Endpoints.LOGIN));
 
-                body("error", equalTo("Internal Server Error"));
+    }
+
+    @Then("The response should contain an error message")
+    public void theResponseShouldContainAnErrorMessage() {
+
+        response.then(). log().all().
+                statusCode(200).
+                header("Content-Type", "application/json; charset=UTF-8").
+                header("connection", "keep-alive").
+                body("status", equalTo(422)).
+                body("message", equalTo("Please Fill in all Required Fields!"));
 
     }
 }
--- src/test/resources/api/login2.feature
+++ src/test/resources/api/login2.feature
@@ -3,8 +3,8 @@ Feature: Testing DuoBank API
   Background:
     Given The Base URI is set
 
-
-  Scenario:
+  @api1
+  Scenario:  Verify test Post login with invalid credentials
     When I send a Post request to the endpoint with wrong credentials
     Then The response should contain an error message
 
